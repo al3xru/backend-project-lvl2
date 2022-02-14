78ade1ed0579a61a1da6b5e263cce0ced8a0273d
Alex Zhukalin <alex@zhukalin.ru>
2022-02-14 18:05:27 +0300

    fixed gendiff executable

--- src/gendiff.js
+++ src/gendiff.js
@@ -8,7 +8,7 @@ export default () => {
     .name('genDiff')
     .description(' Compares two configuration files and shows a difference.')
     // .version(packageInfo.version)
-    .version('0.0.1')
+    .version('0.0.2')
     .arguments('<filepath1> <filepath2>')
     .option('-f, --format <type>', 'output format')
     .parse();
